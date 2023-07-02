13222c6e59cd7213da654ce85b3095556230d7f9
ando <shin.ando@fox-hound.tech>
2023-07-02 18:40:15 +0900

    itan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,18 +10,7 @@
 <body style="margin: 0;">
 	<div id="app">
 		<script src="{{ mix('js/app.js') }}"></script>
-		<script>
-			ReactDOM.render( < Header / > , document.getElementById('header'));
-		</script>
-		<!-- その他のコンテンツ -->
 	</div>
-	<script>
-		ReactDOM.render( < TopScreen / > , document.getElementById('top-screen'));
-	</script>
-	<section>
-	</section>
-	<footer>
-	</footer>
 </body>
 
 </html>
